0acd8455d4f9882718fce9c6e1845126621aebdc
Matt0911 <Mattmanhardt@Gmail.com>
2017-01-21 12:51:55 -0700

    decryption and cleanup

--- src/AES.java
+++ src/AES.java
@@ -5,63 +5,30 @@ public class AES {
 	public static void main(String[] args) {
 
 		AES aes = new AES();
-//		int[][] input = new int[4][4];
-//		input[0][0] = 0x32;
-//		input[0][1] = 0x88;
-//		input[0][2] = 0x31;
-//		input[0][3] = 0xe0;
-//		input[1][0] = 0x43;
-//		input[1][1] = 0x5a;
-//		input[1][2] = 0x31;
-//		input[1][3] = 0x37;
-//		input[2][0] = 0xf6;
-//		input[2][1] = 0x30;
-//		input[2][2] = 0x98;
-//		input[2][3] = 0x07;
-//		input[3][0] = 0xa8;
-//		input[3][1] = 0x8d;
-//		input[3][2] = 0xa2;
-//		input[3][3] = 0x34;
-
-//		int[][] key = new int[4][4];
-//		key[0][0] = 0x2b;
-//		key[0][1] = 0x28;
-//		key[0][2] = 0xab;
-//		key[0][3] = 0x09;
-//		key[1][0] = 0x7e;
-//		key[1][1] = 0xae;
-//		key[1][2] = 0xf7;
-//		key[1][3] = 0xcf;
-//		key[2][0] = 0x15;
-//		key[2][1] = 0xd2;
-//		key[2][2] = 0x15;
-//		key[2][3] = 0x4f;
-//		key[3][0] = 0x16;
-//		key[3][1] = 0xa6;
-//		key[3][2] = 0x88;
-//		key[3][3] = 0x3c;
 		
 		String textInput = "00112233445566778899aabbccddeeff";
+		
 //		String textKey = "2b7e151628aed2a6abf7158809cf4f3c";
-//		String textKey = "000102030405060708090a0b0c0d0e0f1011121314151617";
 //		String textKey = "603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4";
+		
+		/* C.1 AES-128 */
+//		String textKey = "000102030405060708090a0b0c0d0e0f";		
+		
+		/* C.2 AES-192 */
+//		String textKey = "000102030405060708090a0b0c0d0e0f1011121314151617";
+
+		/* C.3 AES-256 */
 		String textKey = "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f";
 		
 		System.out.printf("PLAINTEXT:\t\t%s\n", textInput);
-		System.out.printf("KEY:\t\t\t%s\n", textKey);
+		System.out.printf("KEY:\t\t\t%s\n\n", textKey);
 		int[][] input = aes.textToBlock(textInput, 4);
-		int[][] key = aes.textToBlock(textKey, 8);
+		int nk = textKey.length() / 8;
+		int[][] key = aes.textToBlock(textKey, nk);
 		
-		aes.cipher(key, 8, input);
+		int[][] data = aes.cipher(key, nk, input);
+		data = aes.invCipher(nk, data);
 		
-//		int[][] result = aes.keyExpansion(bytes, 4);
-//		result = aes.shiftRows(result);
-//		result = aes.mixColumns(result);
-//		for (int i = 0; i < result.length; i++) {
-//			for (int j = 0; j < result[0].length; j++) {
-//				System.out.println(Integer.toHexString(result[i][j]));
-//			}
-//		}
 	}
 	
 	public static final int[][] sbox = {{0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76}, {0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0}, {0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15}, {0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75}, {0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84}, {0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf}, {0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8}, {0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2}, {0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73}, {0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb}, {0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79}, {0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08}, {0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a}, {0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e}, {0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf}, {0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16}};
@@ -83,6 +50,8 @@ public class AES {
 		        0xc6, 0x97, 0x35, 0x6a, 0xd4, 0xb3, 0x7d, 0xfa, 0xef, 0xc5, 0x91, 0x39, 0x72, 0xe4, 0xd3, 0xbd,
 		        0x61, 0xc2, 0x9f, 0x25, 0x4a, 0x94, 0x33, 0x66, 0xcc, 0x83, 0x1d, 0x3a, 0x74, 0xe8, 0xcb};
 	
+	private int[][] expandedKey = null;
+	
 	private int[][] cipher(int[][] key, int nk, int[][] data) {
 		System.out.println("CIPHER (ENCRYPT):");
 		int nr = 0;
@@ -91,59 +60,87 @@ public class AES {
 		else if (nk == 8) nr = 14;
 		
 		System.out.printf("round[ 0].input\t\t%s\n", getBlockString(data));
-		int[][] expandedKey = keyExpansion(key, nk, nr);
+		expandedKey = keyExpansion(key, nk, nr);
 		
 		data = addRoundKey(data, key);
 		System.out.printf("round[ 0].k_sch\t\t%s\n", getBlockString(key));
 		
 		for (int i = 1; i < nr; i++) {
 			System.out.printf("round[%d].start\t\t%s\n", i, getBlockString(data));
-//			System.out.println("Start of Round:");
-//			printBlock(data);
 			
 			data = subBytes(data);
 			System.out.printf("round[%d].s_box\t\t%s\n", i, getBlockString(data));
-//			System.out.println("After SubBytes:");
-//			printBlock(data);
 			
 			data = shiftRows(data);
 			System.out.printf("round[%d].s_row\t\t%s\n", i, getBlockString(data));
-//			System.out.println("After ShiftRows:");
-//			printBlock(data);
 			
 			data = mixColumns(data);
 			System.out.printf("round[%d].m_col\t\t%s\n", i, getBlockString(data));
-//			System.out.println("After MixColumns:");
-//			printBlock(data);
 			
 			int[][] roundKey = getRoundKey(expandedKey, i);
 			System.out.printf("round[%d].k_sch\t\t%s\n", i, getBlockString(roundKey));
-//			System.out.println("Round Key Value:");
-//			printBlock(roundKey);
 			data = addRoundKey(data, roundKey);
 		}
+		System.out.printf("round[%d].start\t\t%s\n", nr, getBlockString(data));
 		
 		data = subBytes(data);
 		System.out.printf("round[%d].s_box\t\t%s\n", nr, getBlockString(data));
-//		System.out.println("After SubBytes:");
-//		printBlock(data);
 		
 		data = shiftRows(data);
 		System.out.printf("round[%d].s_row\t\t%s\n", nr, getBlockString(data));
-//		System.out.println("After ShiftRows:");
-//		printBlock(data);
 		
 		int[][] roundKey = getRoundKey(expandedKey, nr);
 		System.out.printf("round[%d].k_sch\t\t%s\n", nr, getBlockString(roundKey));
-//		System.out.println("Round Key Value:");
-//		printBlock(roundKey);
 		data = addRoundKey(data, roundKey);
 		
-		System.out.printf("round[%d].output\t%s\n", nr, getBlockString(data));
-//		System.out.println("Output:");
-//		System.out.println(getBlockString(data));
-//		printBlock(data);
+		System.out.printf("round[%d].output\t%s\n\n", nr, getBlockString(data));
 		return data;
+	}
+	
+	private int[][] invCipher(int nk, int[][] data) {
+		System.out.println("INVERSE CIPHER (DECRYPT):");
+		int nr = 0;
+		if (nk == 4) nr = 10;
+		else if (nk == 6) nr = 12;
+		else if (nk == 8) nr = 14;
+		
+		System.out.printf("round[00].iinput\t%s\n", getBlockString(data));
+		
+		int[][] roundKey = getRoundKey(expandedKey, nr);
+		data = addRoundKey(data, roundKey);
+		System.out.printf("round[00].ik_sch\t%s\n", getBlockString(roundKey));
+		
+		for (int i = 1; i < nr; i++) {
+			System.out.printf("round[%02d].istart\t%s\n", i, getBlockString(data));
+			
+			data = invShiftRows(data);
+			System.out.printf("round[%02d].is_row\t%s\n", i, getBlockString(data));
+			
+			data = invSubBytes(data);
+			System.out.printf("round[%02d].is_box\t%s\n", i, getBlockString(data));
+			
+			roundKey = getRoundKey(expandedKey, nr-i);
+			System.out.printf("round[%02d].ik_sch\t%s\n", i, getBlockString(roundKey));
+			data = addRoundKey(data, roundKey);
+			System.out.printf("round[%02d].ik_add\t%s\n", i, getBlockString(data));
+			
+			data = invMixColumns(data);
+		}
+		System.out.printf("round[%02d].istart\t%s\n", nr, getBlockString(data));
+		
+		data = invShiftRows(data);
+		System.out.printf("round[%02d].is_row\t%s\n", nr, getBlockString(data));
+		
+		data = invSubBytes(data);
+		System.out.printf("round[%02d].is_box\t%s\n", nr, getBlockString(data));
+		
+		roundKey = getRoundKey(expandedKey, 0);
+		System.out.printf("round[%02d].ik_sch\t%s\n", nr, getBlockString(roundKey));
+		data = addRoundKey(data, roundKey);
+		
+		System.out.printf("round[%02d].ioutput\t%s\n", nr, getBlockString(data));
+		return data;
+
 	}
 	
 	private int[][] keyExpansion(int[][] key, int nk, int nr) {		
@@ -210,13 +207,13 @@ public class AES {
 				}
 			}
 			
-			String out = "";
+//			String out = "";
 			for (int j = 0; j < 4; j++) {
 //				System.out.println(Integer.toHexString(newWord[j]));
-				out += String.format("%02x", newWord[j]);
+//				out += String.format("%02x", newWord[j]);
 				expandedKey[j][i] = newWord[j];
 			}
-			System.out.println(out);
+//			System.out.println(out);
 		}
 		
 		return expandedKey;
@@ -295,6 +292,46 @@ public class AES {
 			}
 		}
 		
+		return newState;
+	}
+	
+	private int[][] invShiftRows(int[][]state) {
+		int[][] newState = new int[4][4];
+		
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				int x = (j + i) % 4;
+				newState[i][x] = state[i][j];
+			}
+		}
+		
+		return newState;
+	}
+	
+	private int[][] invSubBytes(int[][] state) {
+		int[][] newState = new int[4][4];
+		
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				int x = (state[i][j] & 0xf0) >> 4;
+				int y = state[i][j] & 0x0f;
+				newState[i][j] = invsbox[x][y];
+			}
+		}
+		
+		return newState;
+	}
+	
+	private int[][] invMixColumns(int[][] state) {
+		int[][] newState = new int[4][4];
+		
+		for (int y= 0; y < 4; y++) {
+			newState[0][y] = ffMultiply(0x0e, state[0][y]) ^ ffMultiply(0x0b, state[1][y]) ^ ffMultiply(0x0d, state[2][y]) ^ ffMultiply(0x09, state[3][y]);
+			newState[1][y] = ffMultiply(0x09, state[0][y]) ^ ffMultiply(0x0e, state[1][y]) ^ ffMultiply(0x0b, state[2][y]) ^ ffMultiply(0x0d, state[3][y]);
+			newState[2][y] = ffMultiply(0x0d, state[0][y]) ^ ffMultiply(0x09, state[1][y]) ^ ffMultiply(0x0e, state[2][y]) ^ ffMultiply(0x0b, state[3][y]);
+			newState[3][y] = ffMultiply(0x0b, state[0][y]) ^ ffMultiply(0x0d, state[1][y]) ^ ffMultiply(0x09, state[2][y]) ^ ffMultiply(0x0e, state[3][y]);
+		}
+
 		return newState;
 	}
 	
